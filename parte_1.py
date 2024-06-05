ff668e926b791603ac9f68445f0d673c567da84d
aiko marques <mutantetrakor@gmail.com>
2024-06-05 14:53:56 -0300

    parte 1 sobre o projeto

---
+++ parte_1.py
@@ -0,0 +1,13 @@
+lista_npcs = [
+    {"name": "Monstro 1", "level": 1},
+    {"name": "Monstro 2", "level": 2},
+    {"name": "Monstro 3", "level": 3},
+    {"name": "Monstro 4", "level": 4},
+    {"name": "Monstro 5", "level": 5},
+    ]
+
+for npc in lista_npcs: #mudou o nome das variaveis na lista para npc
+    print (f"Nome: {npc['name']}")
+
+
+
